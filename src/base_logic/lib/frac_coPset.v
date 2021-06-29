936f0893263b92c7752c4c4a2c7aefe26fc20753
Joseph Tassarotti <joseph.tassarotti@bc.edu>
2021-06-29 19:54:44 -0400

    Reservation lemmas for frac_coPset

--- src/base_logic/lib/frac_coPset.v
+++ src/base_logic/lib/frac_coPset.v
@@ -107,3 +107,131 @@ Section frac_coPset_cmra.
   Qed.
   Canonical Structure frac_coPsetUR := Ucmra frac_coPset frac_coPset_ucmra_mixin.
 End frac_coPset_cmra.
+
+Section frac_coPset_cmra_lemmas.
+
+  Definition fCoPset (q : Qp) (E : coPset) : frac_coPsetR :=
+    {| fc_carrier := λ p, if decide (p ∈ E) then Some q else None |}.
+
+  Lemma fCoPset_valid1 (q : Qp) (E : coPset) :
+    ✓ (fCoPset q E) → set_infinite (⊤ ∖ E).
+  Proof.
+    destruct 1 as (Hval&Hdom). destruct Hdom as (E'&Hp&Hcofin).
+    assert (⊤ ∖ E' ⊆ ⊤ ∖ E).
+    { cut (E ⊆ E'); first by set_solver.
+      intros i Hin. apply Hp. exists q => //=.
+      destruct decide; naive_solver. }
+    eapply set_infinite_subseteq; eauto.
+  Qed.
+
+  Lemma fCoPset_valid2 (q : Qp) (E : coPset) :
+    (q ≤ 1)%Qp →
+    set_infinite (⊤ ∖ E) →
+    ✓ fCoPset q E.
+  Proof.
+    intros Hle Hcofin.
+    split.
+    - intros x => //=; destruct (decide (x ∈ E)) => //=.
+    - exists E. split; auto.
+      intros x => //=; destruct (decide (x ∈ E)) => //=.
+      inversion 1; congruence.
+  Qed.
+
+  Lemma fCoPset_op_plus q1 q2 E :
+    fCoPset (q1 + q2) E ≡ fCoPset q1 E ⋅ fCoPset q2 E.
+  Proof. intros x => //=. destruct (decide (x ∈ E)) => //=. Qed.
+
+  Lemma fCoPset_op_union q E1 E2 :
+    E1 ## E2 →
+    fCoPset q (E1 ∪ E2) ≡ fCoPset q E1 ⋅ fCoPset q E2.
+  Proof.
+    intros Hdisj x => //=.
+    repeat (destruct (decide (x ∈ _))) => //=; set_solver.
+  Qed.
+
+  Lemma fCoPset_op_difference q E1 E2 :
+    E1 ⊆ E2 →
+    fCoPset q E2 ≡ fCoPset q E1 ⋅ fCoPset q (E2 ∖ E1).
+  Proof.
+    intros. rewrite -fCoPset_op_union; last set_solver.
+    by rewrite -union_difference_L.
+  Qed.
+
+  Lemma fCoPset_valid_op1 q1 q2 E1 E2 :
+    (1 < q1 + q2)%Qp →
+    ✓ (fCoPset q1 E1 ⋅ fCoPset q2 E2) → E1 ## E2 ∧ set_infinite (⊤ ∖ (E1 ∪ E2)).
+  Proof.
+    intros Hlt (Hval&Hdom).
+    split.
+    * destruct (decide (E1 ## E2)); auto.
+      intros x Hin1 Hin2.
+      specialize (Hval x).
+      rewrite /fCoPset//= in Hval.
+      repeat (destruct (decide _)); try set_solver; [].
+      revert Hval. rewrite -Some_op Some_valid frac_valid => ?.
+      eapply Qp_le_ngt; eauto.
+    * destruct Hdom as (E&Hdom&Hcofin).
+      assert (⊤ ∖ E ⊆ ⊤ ∖ (E1 ∪ E2)).
+      { cut (E1 ∪ E2 ⊆ E); first by set_solver.
+        intros x Hin. apply Hdom => //=.
+        apply op_is_Some.
+        destruct (decide _); first eauto.
+        right.
+        destruct (decide _); first eauto.
+        set_solver.
+      }
+      eapply set_infinite_subseteq; try eassumption.
+  Qed.
+
+  Lemma fCoPset_valid_op2 q1 q2 E1 E2 :
+    (q1 ≤ 1)%Qp →
+    (q2 ≤  1)%Qp →
+    E1 ## E2 ∧ set_infinite (⊤ ∖ (E1 ∪ E2)) → ✓ (fCoPset q1 E1 ⋅ fCoPset q2 E2).
+  Proof.
+    intros Hlt1 Hlt2.
+    intros (Hdisj&Hinf).
+    split.
+    * intros x => //=.
+      repeat (destruct (decide _)); try naive_solver; try set_solver.
+    * exists (E1 ∪ E2). split; auto.
+      intros p. rewrite //=.
+      rewrite op_is_Some.
+      repeat (destruct (decide _)); try naive_solver; try set_solver.
+      destruct 1 as [[]|[]]; congruence.
+  Qed.
+
+  (* Based on dyn_reservation_map *)
+  Lemma frac_coPset_reserve (Q : frac_coPset → Prop) :
+    (∀ E, set_infinite E → Q (fCoPset 1 E)) →
+    ε ~~>: Q.
+  Proof.
+    intros HQ. apply cmra_total_updateP=> n f.
+      rewrite left_id. intros (Hval&Hdom) => /=.
+      destruct Hdom as (Ef&Hdom&Hcofin).
+    edestruct (coPset_split_infinite (⊤ ∖ Ef)) as
+        (E1 & E2 & HEunion & HEdisj & HE1inf & HE2inf); auto.
+    exists (fCoPset 1 E1).
+    split; first by apply HQ. clear HQ.
+    split.
+    - intros x. rewrite frac_coPset_lookup_op //=.
+      destruct (decide _).
+      * destruct (fc_carrier f x) eqn:Heq.
+        ** exfalso. cut (x ∈ Ef); first by set_solver.
+           apply Hdom; eauto.
+        ** rewrite //=.
+      * rewrite left_id //.
+    - exists (E1 ∪ Ef).
+      split.
+      * intros x. rewrite frac_coPset_lookup_op.
+        rewrite op_is_Some. intros [Hl|Hr].
+        ** set_unfold. left. destruct (decide _); eauto.
+           inversion Hl; congruence.
+        ** set_unfold. right. apply Hdom; auto.
+      * assert (⊤ ∖ (E1 ∪ Ef) = E2) as ->; auto.
+        set_solver.
+  Qed.
+  Lemma frac_coPset_reserve' :
+    ε ~~>: (λ x, ∃ E, set_infinite E ∧ x = fCoPset 1 E).
+  Proof. eauto using frac_coPset_reserve. Qed.
+
+End frac_coPset_cmra_lemmas.
