f40e47ffb7b3fce5be5bb976fe5546c80f966696
Tej Chajed <chajed@wisc.edu>
2024-06-27 17:56:50 -0500

    Simplify bitmap_proof to new version

--- src/program_proof/simplefs/bitmap_proof.v
+++ src/program_proof/simplefs/bitmap_proof.v
@@ -3,35 +3,14 @@ From Perennial.program_proof Require Import disk_prelude.
 From Perennial.goose_lang.lib Require Import typed_slice.
 From Perennial.program_proof Require Import disk_lib.
 From Perennial.Helpers Require Import bytes.
+Import Nat.
 
 From Goose.github_com Require Import tchajed.simplefs.
 
 #[local] Unset Printing Projections.
 
-Definition BlkBits: Z := (4096*8).
-
-Section proof.
-Context `{!heapGS Σ}.
-
-Definition is_block_typed (s: Slice.t) (dq: dfrac) (block: Block) :=
-  own_slice_small s byteT dq (vec_to_list block).
-
-Lemma is_block_to_typed s dq block :
-  is_block s dq block = is_block_typed s dq block.
-Proof. reflexivity. Qed.
-
-Definition block_has_bits (block: Block) (bits: list bool) :=
-  Z.of_nat (length bits) = BlkBits ∧
-  ∀ (i: nat) (bit: bool),
-  bits !! i = Some bit →
-  ∃ (b: u8), vec_to_list block !! (i `div` 8)%nat = Some b ∧
-             Z.testbit (uint.Z b) (i `mod` 8)%nat = bit.
-
 Definition bytes_to_bits (data: list u8): list bool :=
   concat (byte_to_bits <$> data).
-
-Definition block_to_bits (block: Block): list bool :=
-  concat (byte_to_bits <$> vec_to_list block).
 
 Definition length_uniform {A: Type} (l: list (list A)) (n: nat) :=
   ∀ i x, l !! i = Some x → length x = n.
@@ -61,6 +40,16 @@ Proof.
   lia.
 Qed.
 
+Lemma Nat_mod_add_modulus n k :
+  ((n + k) `mod` k = n `mod` k)%nat.
+Proof.
+  rewrite (Div0.add_mod n k k).
+  rewrite Div0.mod_same.
+  rewrite Nat.add_0_r.
+  rewrite Div0.mod_mod.
+  reflexivity.
+Qed.
+
 Lemma lookup_concat_uniform {A: Type} (l: list (list A)) (n: nat) :
   length_uniform l n →
   n ≠ 0%nat →
@@ -84,13 +73,11 @@ Proof.
       simpl.
       f_equal.
       f_equal.
-      admit. (* nonlinear arith goal (mod and minus) *)
-Admitted.
+      rewrite -(Nat_mod_add_modulus (i-n) n).
+      replace (i - n + n)%nat with i by lia; auto.
+Qed.
 
-Hint Unfold BlkBits : word.
-Hint Unfold block_bytes : word.
-
-Lemma byte_to_bits_uniform block :
+Lemma bytes_to_bits_uniform block :
   length_uniform (byte_to_bits <$> block) 8%nat.
 Proof.
   intros i x H.
@@ -98,112 +85,82 @@ Proof.
   len.
 Qed.
 
-Hint Resolve byte_to_bits_uniform : core.
+Hint Resolve bytes_to_bits_uniform : core.
 
-Lemma block_to_bits_length block :
-  length (block_to_bits block) = Z.to_nat BlkBits.
+Lemma bytes_to_bits_length data :
+  length (bytes_to_bits data) = (length data * 8)%nat.
 Proof.
-  rewrite /block_to_bits.
+  rewrite /bytes_to_bits.
   rewrite (length_concat_uniform _ 8%nat).
   { len. }
-  apply byte_to_bits_uniform.
+  apply bytes_to_bits_uniform.
 Qed.
 
-Hint Rewrite block_to_bits_length : len.
+Hint Rewrite bytes_to_bits_length : len.
 
-Lemma byte_to_bits_lookup_Some (bytes: list u8) (i: nat) (bit: bool) :
-  concat (byte_to_bits <$> bytes) !! i = Some bit →
+Lemma bytes_to_bits_lookup_Some (bytes: list u8) (i: nat) (bit: bool) :
+  bytes_to_bits bytes !! i = Some bit →
   ∃ (b: u8), bytes !! (i `div` 8)%nat = Some b ∧
              byte_to_bits b !! (i `mod` 8)%nat = Some bit.
 Proof.
-  intros Hlookup.
+  rewrite /bytes_to_bits => Hlookup.
   erewrite lookup_concat_uniform in Hlookup; eauto.
   apply bind_Some in Hlookup as [bits [Hlookup' Hlookup_bit]].
   fmap_Some in Hlookup' as b. exists b.
   eauto.
 Qed.
 
-Lemma block_has_to_bits block:
-  block_has_bits block (block_to_bits block).
-Proof.
-  split.
-  + len.
-  + intros ?? Hlookup.
-    destruct (list_lookup_lt _ block (i `div` 8)) as [b Hlookup2].
-    { apply lookup_lt_Some in Hlookup.
-      autorewrite with len in Hlookup.
-      apply Nat.Div0.div_lt_upper_bound.
-      move: Hlookup; len. }
-    exists b; split; [ by auto | ].
-    apply byte_to_bits_lookup_Some in Hlookup as [b' [Hlookup' Hlookup_bit]].
-    assert (b = b') by congruence; subst.
-    rewrite /byte_to_bits in Hlookup_bit.
-    fmap_Some in Hlookup_bit.
-    apply lookup_seqZ in Hlookup_bit; intuition subst.
-    f_equal; lia.
-Qed.
+Section proof.
+Context `{!heapGS Σ}.
 
-Lemma block_to_bits_ok block :
-  ∀ bits, block_has_bits block bits ↔ bits = block_to_bits block.
-Proof.
-  intros. split.
-  - intros H.
-    destruct H as [Hlen Hbits].
-    eapply list_eq_same_length; auto.
-    { len. }
-    len. intros ??? Hi Hlookup1 Hlookup2.
-    apply Hbits in Hlookup1 as [b [Hlookup1 Htestbit]].
-    rewrite /block_to_bits in Hlookup2.
-    (* this proof is harder than it's worth *)
-    admit.
-  - intros ->.
-    apply block_has_to_bits.
-Abort.
+Definition own_bitmap (bb: val) (bits: list bool): iProp Σ :=
+  ∃ (s: Slice.t) data,
+    "%Hval" ∷ ⌜bb = (slice_val s, #())%V⌝ ∗
+    "Hs" ∷ own_slice_small s byteT (DfracOwn 1) data ∗
+    "%Hoverflow" ∷ ⌜Z.of_nat (length data) < 2^56⌝ ∗
+    "%Hbits" ∷ ⌜bits = bytes_to_bits data⌝.
 
-Definition own_bitmap (bb: val) (data: list u8) (bits: list bool): iProp Σ :=
-  ∃ (s: Slice.t), ⌜bb = (slice_val s, #())%V⌝ ∗
-                   own_slice_small s byteT (DfracOwn 1) data ∗
-                   ⌜bits = block_to_bits data⌝.
-
-Lemma wp_newBitmap s block :
-  {{{ is_block s (DfracOwn 1) block }}}
+Lemma wp_newBitmap (s: Slice.t) (bits: list bool) :
+  {{{ ∃ data, own_slice_small s byteT (DfracOwn 1) data ∗
+              ⌜bytes_to_bits data = bits⌝ }}}
     newBitmap (slice_val s)
-  {{{ v bits, RET v; own_bitmap v block bits }}}.
+  {{{ v, RET v; own_bitmap v bits }}}.
 Proof.
-  (*@ func newBlockBitmap(block disk.Block) bitmap {                     @*)
-  (*@     return bitmap{block: block}                                    @*)
-  (*@ }                                                                       @*)
-  iIntros (Φ) "Hpre HΦ".
+  iIntros (Φ) "Hpre HΦ". iDestruct "Hpre" as (data) "(Hs & %)". subst.
   wp_rec.
+  iDestruct (own_slice_small_sz with "Hs") as %Hsz.
+  wp_apply wp_slice_len.
+  wp_apply wp_Assume.
+  rewrite bool_decide_eq_true.
+  iIntros (Hoverflow).
   wp_pures.
-  iApply ("HΦ" $! _ (block_to_bits block)).
-  iModIntro.
-  iFrame.
+  iApply "HΦ". iModIntro.
+  rewrite /own_bitmap. iExists _, _; iFrame.
   iPureIntro.
-  split; [ by eauto | ].
-  split.
-  - len.
-  - (* apply block_has_to_bits. *) auto.
+  split; auto.
+  split; auto.
+  revert Hoverflow.
+  change (uint.Z (word.slu (W64 1) (W64 56))) with (2^56).
+  word.
 Qed.
 
 Lemma symex_wp_bitmap__Set s (bytes: list u8) (i : u64) :
-  {{{ own_slice_small s byteT (DfracOwn 1) bytes ∗ ⌜length bytes = 4096%nat⌝ ∗ ⌜uint.Z i < BlkBits⌝ }}}
+  {{{ own_slice_small s byteT (DfracOwn 1) bytes ∗
+        ⌜Z.of_nat (length bytes) < 2^56⌝ ∗
+        ⌜uint.Z i < Z.of_nat (length bytes) * 8⌝ }}}
     bitmap__Set (s, #())%V #i
   {{{ (b: w8), RET #();
       ⌜bytes !! (uint.nat i `div` 8)%nat = Some b⌝ ∗
       own_slice_small s byteT (DfracOwn 1)
-                 (<[(uint.nat i `div` 8)%nat:=word.or b
-                                                    (word.slu
-                                                       (W8 1)
-                                                       (W8 (uint.Z i `mod` 8)))]>
+                 (<[(uint.nat i `div` 8)%nat:=
+                      word.or b
+                        (word.slu (W8 1) (W8 (uint.Z i `mod` 8)))]>
                bytes) }}}.
 Proof.
-  iIntros (Φ) "Hpre HΦ". iDestruct "Hpre" as "(Hbb & %Hbytelen & %Hbound)".
+  iIntros (Φ) "Hpre HΦ". iDestruct "Hpre" as "(Hbb & %Hoverflow & %Hbound)".
   wp_rec. wp_pures.
   wp_apply wp_slice_len. wp_pures.
   iDestruct (own_slice_small_sz with "Hbb") as %Hsz.
-  assert (uint.Z (Slice.sz s) = 4096).
-  { word. }
   rewrite bool_decide_eq_false_2.
   2: {
     rewrite word.unsigned_mul_nowrap; try word.
@@ -232,15 +189,19 @@ Proof.
   iFrame.
 Qed.
 
-Local Ltac Zify.zify_post_hook ::= Z.div_mod_to_equations.
-
-Lemma own_slice_small_to_block s dq (l: list u8) :
-  length l = Z.to_nat 4096 →
-  own_slice_small s byteT dq l = is_block s dq (list_to_block l).
+Lemma split_div_mod_8 (i: nat) :
+  ∃ (i1 i2: nat),
+    (i `div` 8 = i1 ∧
+    i `mod` 8 = i2 ∧
+    i = 8 * i1 + i2 ∧
+    8 * i1 ≤ i ∧
+    0 ≤ i2 < 8)%nat.
 Proof.
-  intros Hlen.
-  rewrite is_block_to_typed /is_block_typed.
-  rewrite list_to_block_to_list //.
+  eexists _, _; split_and!; [ eauto | eauto | .. ].
+  - rewrite {1}(Nat.div_mod i 8) //.
+  - apply Nat.Div0.mul_div_le; lia.
+  - apply Nat.mod_bound_pos; lia.
+  - apply Nat.mod_bound_pos; lia.
 Qed.
 
 Lemma Some_inv {A} (a b: A) : Some a = Some b → a = b.
@@ -249,14 +210,17 @@ Proof. inversion 1; auto. Qed.
 Lemma Some_eq_iff {A} (a b: A) : Some a = Some b ↔ a = b.
 Proof. split; [ apply Some_inv | congruence ]. Qed.
 
+Local Ltac Zify.zify_post_hook ::= Z.div_mod_to_equations.
+
 Transparent w8_instance.w8.
 Lemma set_bit_get (b: w8) (i: u64) bit :
   byte_to_bits (word.or b (word.slu (W8 1) (W8 (uint.Z i `mod` 8))))
   !! Z.to_nat (uint.Z i `mod` 8) = Some bit → true = bit.
 Proof.
-    byte_cases b; bit_off_cases (uint.Z i `mod` 8); compute;
+    byte_cases b; bit_off_cases (uint.Z i `mod` 8);
       intros H%Some_inv; exact H.
 Qed.
+Opaque w8_instance.w8.
 
 Lemma byte_to_bits_lookup (b: u8) (i: nat) :
   byte_to_bits b !! (i `mod` 8)%nat =
@@ -273,125 +237,112 @@ Proof.
   reflexivity.
 Qed.
 
-Lemma set_get_bit_ne (i : w64) (b : w8) (i' : nat) (bit1 bit2 : bool) :
-  (Z.to_nat (uint.Z i `mod` 8) ≠ i' `mod` 8)%nat →
-  byte_to_bits (word.or b (word.slu (W8 1) (W8 (uint.Z i `mod` 8))))
-    !! (i' `mod` 8)%nat = Some bit2
-  → byte_to_bits b !! (i' `mod` 8)%nat = Some bit1 → bit1 = bit2.
-Proof.
-  clear Σ heapGS0.
-  intros Hmodneq.
-  rewrite byte_to_bits_lookup.
-  rewrite byte_to_bits_lookup.
-  rewrite !Some_eq_iff. intros <- <-.
+Opaque Nat.div Nat.modulo.
 
-  move: Hmodneq.
-  assert (i' `mod` 8 < 8)%nat as Hbound.
-  { pose proof (Nat.mod_bound_pos i' 8%nat). lia. }
-  bit_off_cases (i' `mod` 8)%nat;
-    bit_off_cases (uint.Z i `mod` 8);
-    try (intros H; compute in H; exfalso; congruence);
-    byte_cases b;
-    compute;
-    auto.
+Lemma testbit_set_bit (b: w8) (i: Z) (i': nat) :
+  0 ≤ i < 8 →
+  (i' < 8)%nat →
+  Z.testbit (uint.Z (word.or b (word.slu (W8 1) (W8 i)))) i' =
+  if decide (i = Z.of_nat i') then true else Z.testbit (uint.Z b) i'.
+Proof.
+  intros Hbound Hbound'.
+  byte_cases b; bit_off_cases i; bit_off_cases i';
+    try reflexivity.
 Qed.
 
-Opaque w8_instance.w8.
-
-Lemma byte_bit_update (data : list u8) (i : w64) (b : w8) :
-  data !! (uint.nat i `div` 8)%nat = Some b
-  → <[uint.nat i:=true]> (concat (byte_to_bits <$> data)) =
-      concat
-        (byte_to_bits <$>
-          <[(uint.nat i `div` 8)%nat
-            :=word.or b
-                (word.slu (W8 1) (W8 (uint.Z i `mod` 8)))]>
-            data).
+Lemma mod_8_bound (n: nat) :
+  (n mod 8 < 8)%nat.
 Proof.
-  intros Hbyte_lookup.
-  eapply (list_eq_same_length _ _ (length data * 8)%nat); [ | | ].
-  { erewrite -> !length_concat_uniform by eauto.
-    len. }
-  { autorewrite with len.
-    erewrite -> !length_concat_uniform by eauto.
-    len. }
-  intros i' bit1 bit2 Hbound Hlookup1 Hlookup2.
-  apply byte_to_bits_lookup_Some in Hlookup2 as [b' [Hlookupb' Hb']].
+  apply Nat.mod_bound_pos; lia.
+Qed.
 
+Hint Resolve mod_8_bound : core.
+Hint Resolve mod_8_bound : word.
+
+Lemma bytes_to_bits_set:
+  ∀ (i : w64) (data : list w8),
+  uint.Z i < length (bytes_to_bits data)
+  → length data < 2 ^ 56
+  → ∀ b : w8,
+  data !! (uint.nat i `div` 8)%nat = Some b
+  → <[uint.nat i:=true]> (bytes_to_bits data) =
+      bytes_to_bits
+        (<[(uint.nat i `div` 8)%nat:=word.or b
+                                       (word.slu (W8 1) (W8 (uint.Z i `mod` 8)))]>
+           data).
+Proof.
+  intros i data Hbound Hoverflow b Hget_b.
+  eapply (list_eq_same_length _ _ (length data * 8)%nat); [ | | ].
+  { len. }
+  { len. }
+  pose proof (lookup_lt_Some _ _ _ Hget_b) as Hbound'.
+  intros i' bit1 bit2 Hi'_bound Hget1 Hget2.
+  apply bytes_to_bits_lookup_Some in Hget2 as (b2 & Hget2 & Hb2).
   destruct (decide (uint.nat i `div` 8 = i' `div` 8)%nat).
   - (* same byte *)
-    rewrite -e in Hlookupb'.
-    rewrite list_lookup_insert in Hlookupb'.
-    2: {
-      apply lookup_lt_Some in Hbyte_lookup.
-      word.
-    }
+    rewrite <- e in *.
+    rewrite list_lookup_insert in Hget2; [ | len ].
+    inv Hget2.
 
     destruct (decide (uint.nat i = i')); subst.
     + (* same bit *)
-      rewrite list_lookup_insert in Hlookup1.
-      2: {
-        erewrite length_concat_uniform; [ | eauto ]. len.
-      }
-      inversion Hlookup1; subst.
-      inversion Hlookupb'; subst; clear Hlookupb'.
-      move: Hb'.
-      replace (uint.nat i `mod` 8)%nat with (Z.to_nat (uint.Z i `mod` 8)).
-      2: {
-        rewrite Z2Nat.inj_mod //; word.
-      }
-      apply set_bit_get.
+      rewrite list_lookup_insert in Hget1; [ | by len ].
+      inv Hget1.
+      replace (uint.nat i `mod` 8)%nat with (Z.to_nat (uint.Z i `mod` 8)) in *.
+      2: { rewrite Z2Nat.inj_mod //; word. }
+      eapply set_bit_get; eauto.
     + (* same byte but different bit *)
-      rewrite list_lookup_insert_ne // in Hlookup1.
-      apply byte_to_bits_lookup_Some in Hlookup1 as [b'' [Hlookupb'' Hb'']].
+      rewrite list_lookup_insert_ne // in Hget1.
+      apply bytes_to_bits_lookup_Some in Hget1 as [b'' [Hgetb'' Hb'']].
       assert (b = b'') by congruence; subst.
-      inversion Hlookupb'; subst; clear Hlookupb'.
+      inv Hgetb''.
 
-      eapply set_get_bit_ne; eauto.
-      rewrite Z2Nat.inj_mod //; try word.
-      change (Z.to_nat 8) with 8%nat.
-      intros Heq.
+      rewrite byte_to_bits_lookup in Hb2.
+      rewrite byte_to_bits_lookup in Hb''.
+      inv Hb2.
+      rewrite testbit_set_bit; [ | word | auto ].
+      rewrite decide_False //.
+      intros H.
       contradiction n.
-      admit.
-
+      pose proof (Nat.div_mod (uint.nat i) 8 ltac:(lia)).
+      pose proof (Nat.div_mod i' 8 ltac:(lia)).
+      rewrite H1 H2.
+      rewrite e.
+      f_equal.
+      (* how did we get into this mess? *)
+      rewrite Nat2Z.inj_mod in H.
+      change 8%nat with (Z.to_nat 8).
+      rewrite -(Z2Nat.inj_mod (uint.Z i)); [ | word .. ].
+      rewrite H.
+      rewrite Z2Nat.inj_mod; try word.
+      change (Z.to_nat 8%nat) with 8%nat.
+      change (Z.to_nat 8) with 8%nat.
+      rewrite Nat2Z.id.
+      auto.
   - assert (uint.nat i ≠ i') by congruence.
-    rewrite list_lookup_insert_ne // in Hlookup1.
-    rewrite list_lookup_insert_ne // in Hlookupb'.
-    apply byte_to_bits_lookup_Some in Hlookup1 as [b'' [Hlookupb'' Hb'']].
+    rewrite list_lookup_insert_ne // in Hget1.
+    rewrite list_lookup_insert_ne // in Hget2.
+    apply bytes_to_bits_lookup_Some in Hget1 as [b'' [Hgetb'' Hb'']].
     congruence.
+Qed.
 
-Admitted.
-
-Lemma wp_bitmap__Set v block bits (i : u64) :
-  {{{ own_bitmap v block bits ∗ ⌜uint.Z i < BlkBits⌝ }}}
+Lemma wp_bitmap__Set v (bits: list bool) (i: u64) :
+  {{{ own_bitmap v bits ∗ ⌜uint.Z i < Z.of_nat (length bits)⌝ }}}
     bitmap__Set v #i
-  {{{ block', RET #(); own_bitmap v block' (<[uint.nat i := true]> bits) }}}.
+  {{{ RET #(); own_bitmap v (<[uint.nat i := true]> bits) }}}.
 Proof.
-  (*@ func (b bitmap) Set(i uint64) {                                    @*)
-  (*@     if i >= BLK_BITS {                                                  @*)
-  (*@         panic("out of bounds")                                          @*)
-  (*@     }                                                                   @*)
-  (*@     byteIndex := i / 8                                                  @*)
-  (*@     bitIndex := i % 8                                                   @*)
-  (*@     b.block[byteIndex] |= 1 << bitIndex                                 @*)
-  (*@ }                                                                       @*)
-  iIntros (Φ) "Hpre HΦ". iDestruct "Hpre" as "[Hbb %Hbound]".
-  iDestruct "Hbb" as (s) "(-> & %Hlen & Hblock & %Hbits)".
-  wp_apply (symex_wp_bitmap__Set with "[$Hblock]").
-  { iPureIntro. len. }
-  iIntros (b) "[%Hbyte_lookup Hbb]".
-  rewrite own_slice_small_to_block; [ | len ].
+  iIntros (Φ) "Hpre HΦ". iDestruct "Hpre" as "[Hbm %Hbound]".
+  iNamed "Hbm". subst.
+  wp_apply (symex_wp_bitmap__Set with "[$Hs]").
+  { iPureIntro. split; auto.
+    move: Hbound; len. }
+  iIntros (b) "[%Hget_b Hs]".
   iApply "HΦ".
-  iFrame "Hbb".
-
+  iFrame. iSplit; [ done | ].
+  iSplit.
+  { iPureIntro. len. }
   iPureIntro.
-  split; [ done | ].
-  split; [ by len | ].
-
-  rewrite /block_to_bits.
-  rewrite list_to_block_to_list; [ | len ].
-  rewrite -byte_bit_update //.
-  subst. auto.
+  apply bytes_to_bits_set; auto.
 Qed.
 
 End proof.
