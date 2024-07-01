9c99415874fbcddaac4d34994a65985cecb5537d
Tej Chajed <chajed@wisc.edu>
2024-07-01 14:49:01 -0500

    Add skeleton for alloc proof

---
+++ src/program_proof/simplefs/alloc_proof.v
@@ -0,0 +1,5 @@
+From Perennial.program_proof Require Import proof_prelude.
+From Goose.github_com Require Import tchajed.simplefs.alloc.
+
+From Perennial.program_proof Require Import disk_prelude.
+From Perennial.goose_lang.lib Require Import typed_slice.
--- src/program_proof/simplefs/simplefs_proof.v
+++ src/program_proof/simplefs/simplefs_proof.v
@@ -1,4 +1,9 @@
 From Perennial.program_proof.simplefs Require
   superblock_proof
   bitmap_proof
-  dir_proof.
+  dir_proof
+  alloc_proof.
+
+From Goose.github_com.tchajed.simplefs Require
+  inode
+  filesys.
