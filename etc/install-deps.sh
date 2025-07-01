ee675c2d5cf7aa255bd127af29ec0c29f53a8acb
Tej Chajed <chajed@wisc.edu>
2025-07-01 10:20:25 -0500

    fixes

--- etc/install-deps.sh
+++ etc/install-deps.sh
@@ -2,18 +2,14 @@
 
 set -e
 
-apt-get update -y
-apt-get upgrade -y
-
-ln -fs /usr/share/zoneinfo/America/Chicago /etc/localtime
-DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
-dpkg-reconfigure --frontend noninteractive tzdata
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 # opam dependencies
-apt-get install -y wget patch unzip bzip2 gcc make
+sudo apt-get install -y wget patch unzip bzip2 gcc make
 
 # coq dependencies
-apt-get install -y libgmp-dev pkg-config
+sudo apt-get install -y libgmp-dev pkg-config
 
 # other dependencies
-apt-get install -y sudo git python3 zsh
+sudo apt-get install -y sudo git python3 zsh
